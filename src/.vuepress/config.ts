ac6d4b9f53ec7c70c70610133146813ea5b19fbe
sch246 <980001119@qq.com>
2024-01-17 23:45:54 +0800

    w

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -14,7 +14,7 @@ export default defineUserConfig({
   theme,
 
   // 和 PWA 一起启用
-  // shouldPrefetch: false,
+  shouldPrefetch: false,
 
   // 代码超过6行才显示行号
   markdown: {
@@ -29,20 +29,20 @@ export default defineUserConfig({
         /##(.+)##/g,
         '<span title="你知道的太多了" class="cover">$1</span>'
       );
-      // md.renderer.rules.fence = function(tokens, idx, options, env, self) {
-      //   const token = tokens[idx];
-      //   // 检查语言是否为 "py edit"
-      //   if (token.info.trim() === 'py edit' || token.info.trim() === 'python edit') {
-      //       // 转义单引号，并替换换行符
-      //       const scriptContent = token.content
-      //                               .replace(/'/g, "\\'")
-      //                               .replace(/\n/g, '\\n');
-      //       // 返回定制的 PyScriptEditor 组件
-      //       return `<PyScriptEditor :scriptContent="'${scriptContent}'" />`;
-      //   }
-      //   // 默认渲染方式
-      //   return self.renderToken(tokens, idx, options);
-      // };
+      md.renderer.rules.fence = function(tokens, idx, options, env, self) {
+        const token = tokens[idx];
+        // 检查语言是否为 "py edit"
+        if (token.info.trim() === 'py edit' || token.info.trim() === 'python edit') {
+            // 转义单引号，并替换换行符
+            const scriptContent = token.content
+                                    .replace(/'/g, "\\'")
+                                    .replace(/\n/g, '\\n');
+            // 返回定制的 PyScriptEditor 组件
+            return `<PyScriptEditor :scriptContent="'${scriptContent}'" />`;
+        }
+        // 默认渲染方式
+        return self.renderToken(tokens, idx, options);
+      };
   },
 
   plugins: [
