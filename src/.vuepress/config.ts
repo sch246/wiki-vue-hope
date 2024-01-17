9b52ebbd9d0f518258aaa6138969e9a80e083960
sch246 <980001119@qq.com>
2024-01-17 21:57:43 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -29,20 +29,20 @@ export default defineUserConfig({
         /##(.+)##/g,
         '<span title="你知道的太多了" class="cover">$1</span>'
       );
-      md.renderer.rules.fence = function(tokens, idx, options, env, self) {
-        const token = tokens[idx];
-        // 检查语言是否为 "py edit"
-        if (token.info.trim() === 'py edit' || token.info.trim() === 'python edit') {
-            // 转义单引号，并替换换行符
-            const scriptContent = token.content
-                                    .replace(/'/g, "\\'")
-                                    .replace(/\n/g, '\\n');
-            // 返回定制的 PyScriptEditor 组件
-            return `<PyScriptEditor :scriptContent="'${scriptContent}'"/>`;
-        }
-        // 默认渲染方式
-        return self.renderToken(tokens, idx, options);
-      };
+      // md.renderer.rules.fence = function(tokens, idx, options, env, self) {
+      //   const token = tokens[idx];
+      //   // 检查语言是否为 "py edit"
+      //   if (token.info.trim() === 'py edit' || token.info.trim() === 'python edit') {
+      //       // 转义单引号，并替换换行符
+      //       const scriptContent = token.content
+      //                               .replace(/'/g, "\\'")
+      //                               .replace(/\n/g, '\\n');
+      //       // 返回定制的 PyScriptEditor 组件
+      //       return `<PyScriptEditor :scriptContent="'${scriptContent}'"/>`;
+      //   }
+      //   // 默认渲染方式
+      //   return self.renderToken(tokens, idx, options);
+      // };
   },
 
   plugins: [
