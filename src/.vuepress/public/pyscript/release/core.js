c0ab2246e35ce8e07543ee0039392000421a8588
sch246 <980001119@qq.com>
2024-01-18 18:52:54 +0800

    w

--- src/.vuepress/public/pyscript/release/core.js
+++ src/.vuepress/public/pyscript/release/core.js
@@ -1,2 +1,2 @@
-export{b as PyWorker,T as TYPES,c as config,e as hooks,f as whenDefined}from"./core-H_WvvWSA.js";
+export{b as PyWorker,T as TYPES,c as config,e as hooks,f as whenDefined}from"./core-LfH_8z6O.js";
 //# sourceMappingURL=core.js.map
---
+++ src/.vuepress/public/pyscript/release/deprecations-manager-IFtsRyuu.js
@@ -0,0 +1,2 @@
+import{e}from"./core-LfH_8z6O.js";import{notify as o}from"./error-voTi5ss1.js";function r(){const e=document.querySelectorAll("script");for(const o of e)s(o.src)}function s(e){/\/pyscript\.net\/latest/.test(e)&&o("Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead.")}e.main.onReady.add(r),e.main.onWorker.add(r);
+//# sourceMappingURL=deprecations-manager-IFtsRyuu.js.map
